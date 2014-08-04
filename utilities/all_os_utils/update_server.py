671f7149735c1ffbb61932649bfe53a05f06b466
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2014-08-04 12:25:49 +0100

    Updated the config.json to remove the NeuroML bundles (they are now taken from our Maven repo). Updated the pom.xml to add the testbackend. Updated the update_server script to include an "eclipse" option.

--- utilities/all_os_utils/update_server.py
+++ utilities/all_os_utils/update_server.py
@@ -11,6 +11,8 @@ serverHome = os.environ['SERVER_HOME']
 config = json.loads(open(os.path.join(os.path.dirname(__file__), 'config.json')).read())
 sourcesdir = config['sourcesdir']
 
+
+
 def copyext(targetdir, extension):
 	files = glob.iglob(os.path.join(targetdir, '*.'+extension))
 	for file in files:
@@ -18,17 +20,25 @@ def copyext(targetdir, extension):
 	        shutil.copy2(file, os.path.join(serverHome, 'repository', 'usr'))
 
 def main(argv):
-	serverHome = os.environ['SERVER_HOME']
+    serverHome = os.environ['SERVER_HOME']
+    if len(sys.argv)>1 and sys.argv[1]=='eclipse':
+        print 'Updating the virgo server, Eclipse is used'
+        eclipse=True
+    else:
+        print 'Updating the virgo server, Eclipse is not used'
+        eclipse=False
+    for repo in config['repos']:
+        print 'Copying libraries for' , repo['name']
+        targetdir = os.path.join(sourcesdir, repo['name'], 'target')
+        if not eclipse:
+            copyext(targetdir, 'jar')
+            copyext(targetdir, 'war')
+        copyext(os.path.join(targetdir, 'classes', 'lib'), 'jar')
+        copyext(os.path.join(targetdir, 'classes', 'lib'), 'war')
 
-	for repo in config['repos']:
-		targetdir = os.path.join(sourcesdir, repo['name'], 'target')
-		copyext(targetdir, 'jar')
-		copyext(targetdir, 'war')
-		copyext(os.path.join(targetdir, 'classes', 'lib'), 'jar')
-		copyext(os.path.join(targetdir, 'classes', 'lib'), 'war')
-
-	shutil.copy2(os.path.join(sourcesdir, 'org.geppetto', 'geppetto.plan'), os.path.join(serverHome, 'pickup'))
-	print 'Geppetto build deployed to virgo'
+    if not eclipse:
+	    shutil.copy2(os.path.join(sourcesdir, 'org.geppetto', 'geppetto.plan'), os.path.join(serverHome, 'pickup'))
+    print 'Geppetto build deployed to virgo'
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
