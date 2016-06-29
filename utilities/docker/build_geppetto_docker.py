d2d997dd01c24acf91a2c564cc303e19b4413243
Stephen Larson <stephen.larson@gmail.com>
2016-06-29 01:16:28 -0400

    more comment notes on next steps towards dockerfile

--- utilities/docker/build_geppetto_docker.py
+++ utilities/docker/build_geppetto_docker.py
@@ -48,6 +48,8 @@ for p in geppettomodules:
       execute("docker run -it --rm --name my-maven-project -v " + pwd + "/" + p + ":/usr/src/mymaven -v " + pwd + "/.m2:/root/.m2 -w /usr/src/mymaven maven:3.2-jdk-7 mvn clean install")
 
       #copy files into virgo directories
+      #TODO: shift these so they end up under docker/geppetto and so they can get
+      #      copied in when that Dockerfile gets built
       for file in glob.glob(pwd + "/" + p + "/target/classes/lib/*.[a-z]*"):
         shutil.copy(file, pwd+"/virgo/usr")
       for file in glob.glob(pwd + "/" + p + '/target/*.[a-z]*'):
@@ -56,10 +58,18 @@ for p in geppettomodules:
 #copy plan file into virgo pickup directory
 shutil.copy("org.geppetto/geppetto.plan", "virgo/pickup")
 
-#build the virgo docker image from the custom Dockefile we made
+#build the virgo docker image from the custom Dockerfile we made
 #TODO: Build and push this to docker hub, then get rid of this line
 execute("docker build -t slarson/virgo-tomcat-server:3.6.4-RELEASE-jre-7 "+pwd+"/org.geppetto/utilities/docker/virgo-tomcat-server-3.6.4-RELEASE-jre-7/")
+
+#build the geppetto docker image from the custom Dockerfile we made
+#TODO: Make it so we can actually uncomment this line
+#TODO: Build and push this to docker hub, then get rid of this line
+#execute("docker build -t slarson/geppetto:0.3.0 "+pwd+"/org.geppetto/utilities/docker/geppetto/")
 
 #run the virgo docker image on port 8080 and map the volumes to the virgo directories
 # we populated.
 execute("docker run --name='virgo-tomcat-server' --publish=8080:8080 -v "+pwd+"/virgo/pickup:/home/virgo/pickup -v "+pwd+"/virgo/usr:/home/virgo/repository/usr -t slarson/virgo-tomcat-server:3.6.4-RELEASE-jre-7")
+
+#Ideally, this is all we'd have to run in the end.
+#execute("docker run --name='geppetto' --publish=8080:8080 -t slarson/geppetto:0.3.0")
