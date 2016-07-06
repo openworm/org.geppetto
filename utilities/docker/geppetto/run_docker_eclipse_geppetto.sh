2240f7e221a698a24fa01f75a30623b08f12b8f3
Stephen Larson <stephen.larson@gmail.com>
2016-07-06 14:58:50 -0400

    mods to script

--- utilities/docker/geppetto/run_docker_eclipse_geppetto.sh
+++ utilities/docker/geppetto/run_docker_eclipse_geppetto.sh
@@ -3,7 +3,8 @@
 ./get-sources.sh
 ../virgo-tomcat-server-3.6.4-RELEASE-jre-7/build.sh
 ../virgo-tomcat-server-3.6.4-RELEASE-jre-7/run-without-port-expose.sh
-../eclipse-geppetto-luna-java-7/rebuild.sh
-../eclipse-geppetto-luna-java-7/run-bash.sh
+cd ../eclipse-geppetto-luna-java-7
+./rebuild.sh
+./run-bash.sh
 
 #Inside the docker container, run the update_server.py before launching eclipse
