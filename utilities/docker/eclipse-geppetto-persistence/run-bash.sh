421d6096349439f2fc536d1f8bff3961cd94f1e4
Stephen Larson <stephen.larson@gmail.com>
2016-07-07 18:16:49 -0400

    mods to get mysql working

--- utilities/docker/eclipse-geppetto-persistence/run-bash.sh
+++ utilities/docker/eclipse-geppetto-persistence/run-bash.sh
@@ -1,5 +1,2 @@
 xhost +
-sudo docker run --name geppetto-mysql -e MYSQL_ROOT_PASSWORD=geppetto -d mysql:latest
-sudo docker cp init.sql geppetto-mysql:/
-sudo docker exec geppetto-mysql sh -c "mysql -uroot -p'geppetto' < init.sql"
-sudo docker run -ti --publish=8080:8080 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --link geppetto-mysql:mysql slarson/eclipse-geppetto:persistence sh -c 'cd workspace/org.geppetto.persistence; mvn exec:java -Dexec.mainClass="org.geppetto.persistence.util.DBTestData" -Dexec.classpathScope=runtime; /bin/bash'
+sudo docker run -ti --publish=8080:8080 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix slarson/eclipse-geppetto:persistence sh -c 'cd workspace/org.geppetto.persistence; mvn exec:java -Dexec.mainClass="org.geppetto.persistence.util.DBTestData" -Dexec.classpathScope=runtime; /bin/bash'
