6bfa6d5b87563be446b2f46104053a9ca76ee0e8
Stephen Larson <stephen.larson@gmail.com>
2016-07-19 19:51:09 -0700

    use specific branch at runtime optionally

---
+++ utilities/docker/geppetto/check_update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /home/developer/workspace/org.geppetto/utilities/source_setup
+#if the environment variable is set, check out the latest in that branch and use it
+if [ -n "$BRANCH" ]; then
+python gitall.py pull
+python gitall.py reset
+python gitall.py checkout development
+python gitall.py checkout $BRANCH
+cd /home/developer/workspace/org.geppetto/
+mvn install
+fi
