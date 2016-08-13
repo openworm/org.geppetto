ff86e7805774aef77a9fe0778446152f75ac0cab
Your Name <stephen.larson@gmail.com>
2016-08-14 04:44:08 +0700

    minor improvements to scripts

--- utilities/docker/eclipse-geppetto-persistence/run.sh
+++ utilities/docker/eclipse-geppetto-persistence/run.sh
@@ -1,2 +1,2 @@
 xhost +
-sudo docker run -ti --publish=8080:8080 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix slarson/eclipse-geppetto:persistence
+sudo docker run -d --publish=8080:8080 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --name eclipse-geppetto slarson/eclipse-geppetto:persistence
