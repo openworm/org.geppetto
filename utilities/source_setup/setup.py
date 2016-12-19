3acffc48af32e3e1aa7572a04cd7cd4557338cdc
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-12-19 15:55:54 +0100

    Scripts update

--- utilities/source_setup/setup.py
+++ utilities/source_setup/setup.py
@@ -106,14 +106,14 @@ def main(argv):
 	if custom_repo in yes:
 		for repo in config['repos']:
 			if repo['auto_install'] == "yes":
-				print "Geppetto repository automatically cloned", repo['url']
+				print "Geppetto repository cloned by default", repo['url']
 				subprocess.call(['git','clone',repo['url']], cwd = target_dir)
 				#Once the repos are cloned, write to pom.xml
 				writeToPomXML(repo)	
 				writeToPlan(repo)			
 			else:
-				print "Geppetto repository not automatically cloned", repo['url']
-				print "Would you like to include this repository anyway?"
+				print "Geppetto repository not cloned by default", repo['url']
+				print "Would you like to clone this repository? [y/n]"
 				repository = raw_input().lower()
 			
 				if repository in yes:
@@ -124,7 +124,7 @@ def main(argv):
 	else:
 		for repo in config['repos']:
 			if repo['auto_install'] == "yes":
-				print "Geppetto repository automatically cloned", repo['url']
+				print "Geppetto repository cloned by default", repo['url']
 				subprocess.call(['git','clone',repo['url']], cwd = target_dir)
 				#Once the repos are cloned, write to pom.xml
 				writeToPomXML(repo)	
@@ -132,7 +132,7 @@ def main(argv):
 		
 			
 	#Then add the new repo into geppetto.plan and pom.xml
-	print "All Geppetto repositories copied"
+	print "All Geppetto repositories cloned"
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
