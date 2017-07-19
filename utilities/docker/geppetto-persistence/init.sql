bd5c216a457c4c3c1ad2b90a6dc22a3c5dd46e12
jrmartin <yeshuarmartinez@gmail.com>
2017-07-18 20:05:13 -0700

    test

--- utilities/docker/geppetto-persistence/init.sql
+++ utilities/docker/geppetto-persistence/init.sql
@@ -1,4 +1,4 @@
 create database geppetto;
 create user 'user_name' identified by 'password';
-grant all privileges on geppetto.* to 'geppetto_persistence_container'@'%' identified by 'password';
+grant all privileges on geppetto.* to 'guest1'@'%' identified by 'password';
 flush privileges;
