19fae5f665e8d221846fc2d0028ec2b5ec81edf2
Dario Del Piano <dario@metacell.us>
2019-04-09 11:00:01 +0100

    script to update the frontend deployment due to the new problem with virgo tooling that does not do that through eclipse

---
+++ utilities/source_setup/update_frontend.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#
+#	Deploys the geppetto frontend in staging due to the issue experienced with the new Virgo tooling
+#
+
+import os
+import sys
+import json
+import distutils.core
+import shutil
+import glob
+from subprocess import call
+
+serverHome = os.environ['SERVER_HOME']
+config = json.loads(open(os.path.join(os.path.dirname(__file__), 'config.json')).read())
+sourcesdir = config['sourcesdir']
+repo = {
+        "name": "org.geppetto.frontend",
+        "url": "https://github.com/openworm/org.geppetto.frontend.git",
+        "auto_install": "yes"
+}
+
+
+def copyext(targetdir, extension):
+    files = glob.iglob(os.path.join(targetdir, extension+'-*/*'))
+    for file in files:
+        if os.path.isfile(file):
+            shutil.copy2(file, os.path.join(
+            serverHome, 'stage', repo['name']+'.jar'))
+
+def main(argv):
+    print 'Copying Frontend into staging', repo['name']
+    targetdir = os.path.join(sourcesdir, repo['name'], 'target')
+    copyext(targetdir, 'frontend')
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
